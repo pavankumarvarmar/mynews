c89e43ed7fc212e677d7d61d21f53665955c9f27
pavanvarma <pavan.ruddarraju@icentris.com>
2016-06-26 20:01:19 +0530

    added user authentication through devise

--- app/models/news.rb
+++ app/models/news.rb
@@ -1,4 +1,18 @@
 class News < ActiveRecord::Base
-belongs_to :user
 
+	require 'uri'
+	belongs_to :user
+	validates :news_url,:title,:news_style, presence: true
+	validate :url_format 
+
+
+	def url_format
+		if self.news_url.present?
+			begin
+				errors.add(:news_url, "Invalid url") unless URI(self.news_url).is_a?(URI::HTTP)
+			rescue URI::InvalidURIError
+				errors.add(:news_url, "Invalid url")
+			end
+		end
+	end
 end
